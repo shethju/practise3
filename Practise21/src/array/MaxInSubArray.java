e1c79f95105ecc3440df4dce31baf3ac9bc881ba
jay_sheth <shethju@gmail.com>
2018-12-05 07:32:58 -0800

    maxsub

---
+++ Practise21/src/array/MaxInSubArray.java
@@ -0,0 +1,17 @@
+package array;
+
+public class MaxInSubArray {
+	public static void main(String args[]) {
+		int A[] = {-2,1,-3,4,-1,2,1,-5,4};
+		maxSubArray(A);
+	}
+
+	public static int maxSubArray(int[] A) {
+	    int maxSoFar=A[0], maxEndingHere=A[0];
+	    for (int i=1;i<A.length;++i){
+	    	maxEndingHere= Math.max(maxEndingHere+A[i],A[i]);
+	    	maxSoFar=Math.max(maxSoFar, maxEndingHere);	
+	    }
+	    return maxSoFar;
+	}
+}
